34aa8775a6696e9aa9f8ba01da519a51dfd1014a
Carlos Guedes <cguedes@gmail.com>
2010-06-08 09:54:38 +0100

    Fim da implementação da janela que implementa a conversão entre idade de cães e humanos. Utilização do modelo de programação event-driven.

--- code/GUI/src/ui/DogAgeFrame2.java
+++ code/GUI/src/ui/DogAgeFrame2.java
@@ -1,21 +1,17 @@
 package ui;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Container;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.GridLayout;
-import java.awt.LayoutManager;
-
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
-
-import sun.font.FontFamily;
 
 // Inclui os elementos necessários à conversão 
 // de idade do cão para idade do Humano
 public class DogAgeFrame2 extends JFrame {
 
+	
+	private JTextField txtDogAge, txtHumanAge;
+	
+	
 	public DogAgeFrame2() 
 	{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -28,17 +24,89 @@ public class DogAgeFrame2 extends JFrame {
 		container.setLayout( layout );
 		
 		// Criação da label (bloco de texto)
-		JLabel lblDogYear     = new JLabel("Dog Year: ");
-		JTextField txtDogYear = new JTextField(" « insert dog age here » ");
+		JLabel lblDogAge        = new JLabel("Dog Age: ");
+		txtDogAge    			= new JTextField("10");
+		JButton btnConvertAge   = new JButton("Convert");
+		JLabel lblHumanAge      = new JLabel("Human Age: ");
+		txtHumanAge  			= new JTextField();
+		
+		// Definir caracteristicas dos elementos gráficos
+		txtDogAge.setPreferredSize(new Dimension(100, txtDogAge.getPreferredSize().height));
+		txtHumanAge.setPreferredSize(new Dimension(100, txtDogAge.getPreferredSize().height));
+		
+		// Registar eventos
+		//   -> Registar no "evento de clique" do botão
+		ConvertButtonListener convertButtonListener = new ConvertButtonListener(); 
+			//new ConvertButtonListener(txtDogAge, txtHumanAge);
+		btnConvertAge.addActionListener( convertButtonListener );
+		
+		//   -> Registar no "evento de mouse" do botão
+		ChangeColorMouseListener changeColorMouseListener = new ChangeColorMouseListener();
+		btnConvertAge.addMouseListener(changeColorMouseListener);
+		txtDogAge.addMouseListener(changeColorMouseListener);
+		lblDogAge.setOpaque(true);
+		lblDogAge.addMouseListener(changeColorMouseListener);
+		
 		
 		// Adicionar a label à janela
-		container.add(lblDogYear/*, BorderLayout.WEST*/);
-		container.add(txtDogYear);
+		container.add(lblDogAge/*, BorderLayout.WEST*/);
+		container.add(txtDogAge);
+		container.add(btnConvertAge);
+		container.add(lblHumanAge);
+		container.add(txtHumanAge);
 		
 		// Ajustar a janela à dimensão necessária para apresentar
 		// todos os seus componentes filho, com as suas dimensões
 		// preferidas
 		pack();
 	}
+	
+	private /*static*/ class ConvertButtonListener implements ActionListener {
 
+		/*
+		 * Desnecessário porque a classe deixou de ser STATIC,
+		 *  ou seja, tem acesso às variáveis de instância do
+		 *  objecto (DogAgeFrame2) que a criou. 
+		 * 
+		private final JTextField txtDogAge;
+		private final JTextField txtHumanAge;
+
+		public ConvertButtonListener(JTextField txtDogAge, JTextField txtHumanAge) 
+		{
+			this.txtDogAge = txtDogAge;
+			this.txtHumanAge = txtHumanAge;
+		}
+		*/
+
+		@Override
+		public void actionPerformed(ActionEvent e) 
+		{
+			int humanAge = new Integer(txtDogAge.getText()) * 7;
+			
+			txtHumanAge.setText( "" + humanAge );
+			
+
+		}
+		
+		
+		
+	}
+
+	private class ChangeColorMouseListener extends MouseAdapter /*implements MouseListener*/ 
+	{
+
+		private Color lastColor;
+		
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			lastColor = e.getComponent().getBackground();
+			e.getComponent().setBackground( Color.RED );
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			e.getComponent().setBackground( lastColor );
+		}
+
+	}
 }
